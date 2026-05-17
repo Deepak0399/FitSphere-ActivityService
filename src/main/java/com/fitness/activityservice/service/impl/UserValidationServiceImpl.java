e50916d5f01a96b7d90204d85721807765d495ae
Deepak <deepakkumar97967@gmail.com>
2026-05-17 21:32:02 +0530

    added logging

--- src/main/java/com/fitness/activityservice/service/impl/UserValidationServiceImpl.java
+++ src/main/java/com/fitness/activityservice/service/impl/UserValidationServiceImpl.java
@@ -4,12 +4,14 @@ import com.fitness.activityservice.exception.InvalidRequestException;
 import com.fitness.activityservice.exception.ResourceNotFoundException;
 import com.fitness.activityservice.service.UserValidationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserValidationServiceImpl implements UserValidationService {
 
@@ -17,6 +19,7 @@ public class UserValidationServiceImpl implements UserValidationService {
 
     @Override
     public boolean validateUser(String userId) {
+        log.info("Calling User Validation API for UserId: {}", userId);
         try {
             return Boolean.TRUE.equals(userServiceWebClient.get()
                     .uri("/api/users/{userId}/validate", userId)
